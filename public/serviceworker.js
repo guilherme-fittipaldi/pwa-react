818c40ea3f10baa570bee335b5b05235511b2bbf
guilherme-fittipaldi <guilherme.fittipaldi6@gmail.com>
2021-06-02 10:54:33 -0300

    alterando cache do sw

---
+++ public/serviceworker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'version-1'; 
+const urlsToCache = ['index.html', 'offline.html'];
+
+const self = this;
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) =>{
+        console.log('opened cache');
+
+        return cache.addAll(urlsToCache);
+      })
+  )
+});
+
+self.addEventListener('fetch', (event) => {
+  
+});
+
+self.addEventListener('activate', (event) => {
+  
+});
+
